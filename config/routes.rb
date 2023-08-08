974c883c7e63013bc8aa7115452212de854769eb
Shota Sato <last11142@gmail.com>
2023-08-08 14:07:04 +0900

    [update] fin 7.4

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  resources :users  # 操作を行う対象となるものを設置（テーブルとか？）
 end
